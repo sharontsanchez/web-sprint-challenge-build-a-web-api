4beca0035d1af00b8250308032c43e0ffb6b1a23
Sharon Sanchez <sharontsanchez@gmail.com>
2022-02-11 13:16:50 -0600

    projects-router and actions-router has been set up and connected to api server.js

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,9 @@
 // Write your "actions" router here!
+
+const express = require('express');
+const Actions = require('./actions-model');
+const router = express.Router();
+
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,23 @@
+// imports here
 const express = require('express');
-const server = express();
 
 // Configure your server here
+const server = express();
+
+
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
+
+const actionsRouter = require('./actions/actions-router');
+const projectsRouter = require('./projects/projects-router');
+
+server.use('./api/actions', actionsRouter)
+server.use('./api/projects', projectsRouter)
+
+server.use(express.json());
+
+
+
 // Do NOT `server.listen()` inside this file!
 
 module.exports = server;
